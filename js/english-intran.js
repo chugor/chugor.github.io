2281ad847aaec0812fcab20214d554598e5aa0fa
chugor <jt199884@gmail.com>
2015-09-19 20:53:33 +0800

    Added English

---
+++ js/english-intran.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+	$(".whatP").hide();
+	$(".eg1Ans").hide();
+	$(".eg2Ans").hide();
+	$(".eg3Ans").hide();
+
+	$(".whatBtn").click(function () {
+		$(this).hide();
+		$(".whatP").show(1000);
+	});
+
+	$(".eg1").click(function () {
+		$(this).hide();
+		$(".eg1Ans").show(500);
+	});
+
+	$(".eg2").click(function () {
+		$(this).hide();
+		$(".eg2Ans").show(500);
+	});
+
+	$(".eg3").click(function () {
+		$(this).hide();
+		$(".eg3Ans").show(500);
+	});
+
+	$(".preBtn").click(function () {
+		window.location.assign("../pages/english-tran.html");
+	})
+
+	$(".nextBtn").click(function () {
+		window.location.assign("../pages/english-com.html");
+	});
+
+})
